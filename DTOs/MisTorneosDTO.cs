46f00251e3231ae348febf0672344167ee950540
Axel Aguilar <ramses.coto1@gmail.com>
2026-03-23 21:53:05 -0600

    Escenario 4: Implementación de DTOs

---
+++ DTOs/MisTorneosDTO.cs
@@ -0,0 +1,14 @@
+﻿using Google.Cloud.Firestore;
+
+namespace ExamenII_Web.api.DTOs;
+
+public class MisTorneosDTO
+{
+    public string torneoId { get; set; }
+    public string estado { get; set; }
+    public int posicion { get; set; }
+    public int puntos { get; set; }
+    public int victorias { get; set; }
+    public int derrotas { get; set; }
+    public Timestamp fechaInscripcion { get; set; }
+}
