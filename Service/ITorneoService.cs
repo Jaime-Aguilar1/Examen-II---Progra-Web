1680dcfa50f74ae752e154c1ab68b40b78807351
fabda21 <fadamuor@gmail.com>
2026-03-23 21:38:38 -0600

    Añadiendo Services de Torneo

---
+++ Service/ITorneoService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ExamenII_Web.api.Models;
+using ExamenII_Web.api.DTOs;
+
+namespace ExamenII_Web.api.Services
+{
+    public interface ITorneoService
+    {
+        Task<string> CrearTorneoAsync(TorneoDTO dto, string organizadorId);
+        Task<List<Torneo>> ObtenerTorneosAsync(string juego, string estado, double? minPrecio, double? maxPrecio, int? minNivelReq, int? maxNivelReq, int page, int pageSize);
+        Task<bool> ActualizarTorneoAsync(string id, TorneoUpdateDto dto, string userId, bool isAdmin);
+        Task<bool> CancelarTorneoAsync(string id, string userId, bool isAdmin);
+        Task<bool> CambiarEstadoAsync(string id, string nuevoEstado, string userId, bool isAdmin);
+    }
+}
